a0732e0192b9e180aa8b2bb3686fe93f4f4cedfb
Thomas <thurtig.email@gmail.com>
2023-08-16 17:26:11 +0200

    add imprint address

--- src/pages/Imprint.svelte
+++ src/pages/Imprint.svelte
@@ -5,6 +5,9 @@
 <PageContainer title="Imprint">
   <p>
     Thomas Hurtig<br />
+    c/o IP-Management #15561 <br />
+    Ludwig-Erhard-Straße 18 <br />
+    20459 Hamburg <br /><br />
     E-Mail: <a href="mailto:contact@thomashurtig.de">contact@thomashurtig.de</a>
   </p>
 </PageContainer>
--- src/pages/Privacy.svelte
+++ src/pages/Privacy.svelte
@@ -8,6 +8,9 @@
     <slot slot="heading">Accountability</slot>
     <p>
       Thomas Hurtig <br />
+      c/o IP-Management #15561 <br />
+      Ludwig-Erhard-Straße 18 <br />
+      20459 Hamburg <br /><br />
       E-Mail:
       <a href="mailto:contact@thomashurtig.de">contact@thomashurtig.de</a>
     </p>
