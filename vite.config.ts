c2a5455d13984bccf26c6263e2cbbe92d98810eb
Thomas <thurtig.email@gmail.com>
2023-08-16 17:24:27 +0200

    add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default ({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
   return defineConfig({
+    base: process.env.VITE_BASE_URL,
     optimizeDeps: {
       exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
     },
@@ -66,12 +67,14 @@ export default ({ mode }) => {
           display: "standalone",
           icons: [
             {
-              src: "/icons/android-chrome-192x192.png",
+              src:
+                process.env.VITE_BASE_URL + "icons/android-chrome-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/icons/android-chrome-512x512.png",
+              src:
+                process.env.VITE_BASE_URL + "icons/android-chrome-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
